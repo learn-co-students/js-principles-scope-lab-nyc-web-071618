0285685ecdf51a166d236d572e23f6ddfc56f556
jin li <jinyuli91@gmail.com>
2018-08-25 13:52:48 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = "me";
+
+
+
+const upperCaseCustomerName = (name) =>{
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob'
+}
+
+const overwriteBestCustomer = (name) =>{
+  bestCustomer = name
+}
+
+const changeLeastFavoriteCustomer = () =>{
+   leastFavoriteCustomer = 'jin'
+}
